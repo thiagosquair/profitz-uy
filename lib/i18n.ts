e15c39f6b523dd952cb0979f91af07cee82c58db
thiagosquair <squair@live.com>
2025-06-12 21:38:44 +0100

    Create i18n.ts

---
+++ lib/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(Backend )
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: false,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    ns: ["common"], // default namespace
+    defaultNS: "common",
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    detection: {
+      order: ["localStorage", "navigator", "htmlTag"],
+      caches: ["localStorage"],
+    },
+  });
+
+export default i18n;
