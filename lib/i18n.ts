001df2aaeff6c94f9934abbb456852cc2879b03f
thiagosquair <squair@live.com>
2025-06-13 00:29:21 +0100

    Update i18n.ts

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,10 +1,17 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+
+// Import both backends
+import HttpBackend from "i18next-http-backend";
+import FsBackend from "i18next-fs-backend";
+
+// Determine if we are in the browser environment
+const isBrowser = typeof window !== "undefined";
 
 i18n
-  .use(Backend )
+  // Conditionally use the appropriate backend
+  .use(isBrowser ? HttpBackend : FsBackend )
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -16,7 +23,8 @@ i18n
     ns: ["common"], // default namespace
     defaultNS: "common",
     backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
+      // Adjust loadPath based on environment
+      loadPath: isBrowser ? "/locales/{{lng}}/{{ns}}.json" : "./public/locales/{{lng}}/{{ns}}.json",
     },
     detection: {
       order: ["localStorage", "navigator", "htmlTag"],
