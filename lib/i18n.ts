fa3ebdaacbd6fb5689c1c38d0f42c2012d677b9f
thiagosquair <squair@live.com>
2025-06-13 12:50:43 +0100

    Update i18n.ts

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -3,37 +3,31 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpBackend from 'i18next-http-backend'; // Import for client-side
-import FsBackend from 'i18next-fs-backend'; // Import for server-side
+import HttpBackend from 'i18next-http-backend';
 
-// Import the configuration from next-i18next.config.mjs
-import nextI18nextConfig from '../next-i18next.config.mjs'; // Note the .mjs extension
-
-const isBrowser = typeof window !== 'undefined';
+// next-i18next.config.js is not directly imported here for client-side setup
+// The configuration will be passed via props or context from server-side
 
 i18n
-  .use(initReactI18next )
-  .use(LanguageDetector) // Only use LanguageDetector in the browser
-  .use(isBrowser ? HttpBackend : FsBackend) // Conditionally use HttpBackend or FsBackend
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpBackend) // Always use HttpBackend for client-side
   .init({
-    ...nextI18nextConfig.i18n,
-    // Load resources based on environment
-    backend: {
-      loadPath: isBrowser ? '/locales/{{lng}}/{{ns}}.json' : './public/locales/{{lng}}/{{ns}}.json',
-    },
-    // Fallback language
     fallbackLng: 'en',
-    // Default namespace
     ns: ['common'],
     defaultNS: 'common',
-    // Key separator
     keySeparator: false,
     interpolation: {
-      escapeValue: false, // react already escapes by default
+      escapeValue: false,
     },
     react: {
       useSuspense: false,
     },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
   });
 
 export default i18n;
+
+
