47379b1bda599eeed830b2441e58fe5ec4d004f4
thiagosquair <squair@live.com>
2025-06-13 12:49:29 +0100

    Update I18nProvider.tsx

--- components/I18nProvider.tsx
+++ components/I18nProvider.tsx
@@ -4,5 +4,7 @@ import { I18nextProvider } from "react-i18next"
 import i18n from "@/lib/i18n"
 
 export function I18nProvider({ children }: { children: React.ReactNode }) {
-  return <i18nextProvider I18n={i18n}>{children}</i18nextProvider>
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
 }
+
+
