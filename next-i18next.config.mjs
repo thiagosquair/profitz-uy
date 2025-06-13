3cc368a6a0e6a6956b5aee6c4951c7c804fd1df5
thiagosquair <squair@live.com>
2025-06-13 01:24:51 +0100

    Update next-i18next.config.mjs

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -1,13 +1,15 @@
-// next-i18next.config.js
+// next-i18next.config.mjs
 
-module.exports = {
+const nextI18nextConfig = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'fr'], // Add all your supported locales here
   },
   // This is important for server-side rendering
   // It tells next-i18next where to find your translation files on the server
-  localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+  localePath: typeof window === 'undefined' ? (await import('path')).resolve('./public/locales') : '/locales',
   // Disable debug mode in production
   debug: process.env.NODE_ENV === 'development',
 };
+
+export default nextI18nextConfig;
